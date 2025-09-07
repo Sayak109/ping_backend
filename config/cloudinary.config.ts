702a97637eb4d4215c2576267edf9ba292b28860
Sayak109 <sayakpanda123@gmail.com>
2025-09-07 16:00:05 +0530

    up

--- config/cloudinary.config.ts
+++ config/cloudinary.config.ts
@@ -1,13 +1,5 @@
 import { v2 as cloudinary } from "cloudinary";
 
-console.log("Cloudinary ENV check:", {
-  CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
-  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
-  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET
-    ? "loaded"
-    : "missing",
-});
-
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
